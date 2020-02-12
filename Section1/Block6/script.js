a712525b24d16dd0e8d7fee511d620def383f195
Rustam <Xpyctam4@yandex.ru>
2020-02-12 12:34:55 +0400

    check

--- Section1/Block6/script.js
+++ Section1/Block6/script.js
@@ -1,2 +1,30 @@
-// Import stylesheets
-import './style.css';
+var mySwiper = undefined;
+  function initSwiper() {
+    var screenWidth = window.outerWidth;
+    if ( (screenWidth < (351)) && (mySwiper == undefined)) {
+        swiper = new Swiper('.swiper-container', {
+            // init: false,
+            slidesPerView: 'auto',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                }
+            },
+        });
+    } else if ((screenWidth > 350) && (mySwiper != undefined)) {
+      mySwiper.destroy();
+      mySwiper = undefined;
+      console.log(mySwiper)
+    //   $('.swiper-wrapper').removeAttr('style');
+    //   $('.swiper-slide').removeAttr('style');
+    }
+  }
+  initSwiper();
+
+  window.addEventListener('resize', function() {
+    initSwiper();
+  });
